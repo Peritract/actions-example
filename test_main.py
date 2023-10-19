7afdd62c18ac85fa54dd38aeb0ce73dee5b18990
Dan <peritract@hotmail.co.uk>
2023-10-19 12:12:31 +0100

    Pylint

--- test_main.py
+++ test_main.py
@@ -6,14 +6,15 @@ from main import get_sarcastic_text
 
 
 def test_rejects_non_string_input():
+    """Checks that the function rejects invalid arguments."""
 
     with pytest.raises(TypeError) as ex:
         get_sarcastic_text(34)
-    
+
     assert ex.value.args[0] == "Argument must be a string."
 
 
 def test_returns_expected_values():
-
+    """Checks that the function works as expected."""
     assert get_sarcastic_text("hello") == "hElLo"
     assert get_sarcastic_text("big horse") == "bIg hOrSe"
