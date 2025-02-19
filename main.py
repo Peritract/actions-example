7a2b77668f2123cd2b8b6b780ef5fbca3f7333ac
Dan Keefe <peritract@hotmail.co.uk>
2025-02-19 10:12:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 """Functions to manipulate text in specifically stupid ways."""
 
+from flask import Flask
 
-def get_sarcastic_text(text: str) -> str:
-    """Returns the sarcastic version of a string."""
+app = Flask(__name__)
 
-    if not isinstance(text, str):
-        raise TypeError("Argument must be a string.")
-
-    return "".join([c.lower()
-                    if i % 2 == 0
-                    else c.upper()
-                    for i, c in enumerate(text)])
-
+@app.get("/")
+def index():
+    return "I am working!"
 
 if __name__ == "__main__":
-    print(get_sarcastic_text("Look at me; I'm reading the example code!"))
+    app.run(host="0.0.0.0")
